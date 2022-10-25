37d50542879d0acd9315ce009a44a180ba3147ee
Edwin King'ori <eddmunyiri@gmail.com>
2022-10-25 21:28:58 +0300

    Learning python the hard way exercises

---
+++ exe9.py
@@ -0,0 +1,12 @@
+days = "Mon Tue Wed Thur Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJune\nJuly\nAug"
+
+print("Here are the days:", days)
+print("Here are the months:", mmonths)
+
+print("""
+There's somethign going on here. 
+With the three doube-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+""")
