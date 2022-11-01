3a909e1e15d07539b78d378ce2d711bd4b5fdde8
EDDY <eddmunyiri@gmail.com>
2022-11-01 22:25:06 +0300

    LPTHW

--- exe15.py
+++ exe15.py
@@ -8,7 +8,7 @@ txt = open(filename)
 print(f"Here's your file {filename}")
 print(txt.read)
 
-print(f"Type thhe filename again!")
+print(f"Type the filename again!")
 file_again = input(">")
 
 txt_again = open(file_again)
